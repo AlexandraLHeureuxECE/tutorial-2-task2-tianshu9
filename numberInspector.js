ee503a3dff714a2091c3cfa6423694194433ef42
tianshu9 <135168073+tianshu9@users.noreply.github.com>
2023-11-16 10:48:11 -0500

    Update numberInspector.js

--- numberInspector.js
+++ numberInspector.js
@@ -12,43 +12,49 @@
         // Complete the code so that based on a given string "palindrome, max, duplicate, divisibleBy" 
         // The appropriate function is returned.
 
+        case 'palindrome':
+            return Palindrome;
+        case 'max':
+            return MaxDigit;
+        case 'duplicate':
+            return DuplicateNumber;
+        case 'divisibleBy':
+            return MakeDivisibleBy;
+        
      /*Write a function that returns the palindrome number of the number you are given, 
     *For example, if you are given 1234, it should return 4321. 
     *You are expected to use only the loops/conditional statements we have learned in class. 
     *Keep in mind that you can access each digit of a number through simple operations. 
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a numberr*/
-    case 'palindrome':
-        return function(num) {
-            let reversed = 0;
-            let original = num;
-
-            while (num > 0) {
-                reversed = reversed * 10 + (num % 10);
-                num = Math.floor(num / 10);
-            }
-
-            return reversed === original;
-        };
+    function Palindrome(x){
+        let digit = 0;
+        let palindrome = 0;
+        while(x > 0) {
+            digit = x % 10;
+            palindrome = (palindrome * 10) + digit;
+            x = Math.floor(x/10);
+        }
+        return String(palindrome);
+    }
 
     /* Write a function that will return the maximum digit of the number you are given.
     * For example, if you are given 192345, it should return 9. If you are given 12645 it should return 6. 
     *Keep in mind that you can access each digit of a number through simple operations. 
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a numberr*/
-    case 'max':
-        return function(num) {
-            let maxDigit = 0;
-
-            while (num > 0) {
-                let digit = num % 10;
-                maxDigit = Math.max(maxDigit, digit);
-                num = Math.floor(num / 10);
+    function MaxDigit(x) {
+        let digit = 0;
+        let max = 0;
+        while (x > 0){
+            digit = x % 10;
+            if (digit > max){
+                max = digit;
             }
-
-            return maxDigit;
-
+            x = Math.floor(x/10);
         }
+        return Number(max);
+    }
 
     /*This function can be use to identify whether a number contains 2 or more consecutive duplicate numbers
     * For example, if you are given 1223 it should return yes because we have 22 in our number. Similarly, 
@@ -56,16 +62,19 @@
     *Keep in mind that you can access each digit of a number through simple operations. 
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a number, the remainder operator is also helful*/
-    case 'duplicate':
-        return function(num) {
-            let strNum = num.toString();
-            for (let i = 0; i < strNum.length - 1; i++) {
-                if (strNum[i] === strNum[i+1]) {
-                    return true;
-                }
+    function DuplicateNumber(x) {
+        let xString = String(x);
+        let digit = xString[0];
+        for (let i = 0; i < xString.length; i++) {
+            if (xString[i+1] == digit) {
+                return true;
             }
-            return false;
+            else {
+                digit = xString[i+1];
+            }
         }
+        return false;
+    }
      
     /*This function will return the value needed to be added to a number to make it divisible by the given dividor. 
     *For example, if you are given 63 and 5, we are looking for the number needed to make 63 divisible by 5. In this case, 
@@ -74,14 +83,15 @@
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a number, the remainder operator is also helful*/
 
-    case 'divisibleBy':
-        return function(num, divisor) {
-            let remainder = num % divisor;
-            return remainder === 0 ? 0 : divisor - remainder;
-        };
-
-    default:
-        return null;
+    function MakeDivisibleBy(x, y) {
+        let amount = 0;
+        while(x % y != 0) {
+            amount++;
+            x++;
+        }
+        return Number(amount);
+    }
+    
     }
 }
 
